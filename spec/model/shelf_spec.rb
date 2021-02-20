909ad8dfc7c652d213b160af3b84dcd2ef0201f0
BINUS\anggi.hadaya <anggi.hadaya@binus.edu>
2021-02-21 02:14:09 +0700

    [Anggi] refactoring shelf spec

--- spec/model/shelf_spec.rb
+++ spec/model/shelf_spec.rb
@@ -7,26 +7,24 @@ describe Shelf do
         shelf = Shelf.new('', '', '')
         expect(shelf.valid?).to eq(false)
       end
-
       it 'should return true if shelf is not empty' do
         shelf = Shelf.new('1', '2', '1')
         expect(shelf.valid?).to eq(true)
       end
-
       it 'should return false if shelf attribute more than 100' do
         shelf = Shelf.new('1', '201', '1')
         expect(shelf.valid?).to eq(false)
       end
     end
-    context 'print' do
-      it 'should return double digit if number less than 10' do
-        shelf = Shelf.new(1, 1, 1)
-        expect(shelf.print).to eq('010101')
-      end
-      it 'should return its number if number more than 10' do
-        shelf = Shelf.new(12, 13, 14)
-        expect(shelf.print).to eq('121314')
-      end
+  end
+  context 'print' do
+    it 'should return double digit if number less than 10' do
+      shelf = Shelf.new(1, 1, 1)
+      expect(shelf.print).to eq('010101')
+    end
+    it 'should return its number if number more than 10' do
+      shelf = Shelf.new(12, 13, 14)
+      expect(shelf.print).to eq('121314')
     end
   end
 end
