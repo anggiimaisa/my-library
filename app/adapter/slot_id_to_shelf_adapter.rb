29122c39ded1ebb470428fca710b657d99edd255
Anggi Maisa Hadaya <anggimhadaya@gmail.com>
2021-02-27 19:07:48 +0700

    [Anggi] refactoring slot id to shelf object adapter - convert string to int

--- app/adapter/slot_id_to_shelf_adapter.rb
+++ app/adapter/slot_id_to_shelf_adapter.rb
@@ -2,6 +2,6 @@ require './app/model/shelf'
 
 class SlotIdToShelfAdapter
   def parse(slot_id)
-    Shelf.new(slot_id[0..1], slot_id[2..3], slot_id[4..5])
+    Shelf.new(slot_id[0..1].to_i, slot_id[2..3].to_i, slot_id[4..5].to_i)
   end
 end
--- spec/adapter/slot_id_to_shelf_adapter_spec.rb
+++ spec/adapter/slot_id_to_shelf_adapter_spec.rb
@@ -2,11 +2,12 @@ require './app/adapter/slot_id_to_shelf_adapter'
 
 describe SlotIdToShelfAdapter do
   let(:slot_id) { '010101' }
+  let(:shelf) { Shelf.new(1, 1, 1) }
   context 'parse' do
     it 'should return shelf object' do
       adapter = SlotIdToShelfAdapter.new
-      shelf = adapter.parse(slot_id)
-      expect(shelf.is_a?(Shelf)).to eq(true)
+      actual = adapter.parse(slot_id).eql? shelf
+      expect(actual).to eq(true)
     end
   end
 end
