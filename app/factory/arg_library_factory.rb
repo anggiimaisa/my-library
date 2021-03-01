57cdbbe92675980063591bfcc53dc29eacfe81f8
Anggi Maisa Hadaya <anggimhadaya@gmail.com>
2021-03-01 23:41:48 +0700

    [Anggi] refactoring arg library factory - provides other args class

--- app/factory/arg_library_factory.rb
+++ app/factory/arg_library_factory.rb
@@ -1,6 +1,10 @@
 require './app/factory/library/arg_build_library'
 require './app/factory/library/arg_list_book'
 require './app/factory/library/arg_take_book'
+require './app/factory/library/arg_find_book'
+require './app/factory/library/arg_put_book'
+require './app/factory/library/arg_search_books_by_author'
+require './app/factory/library/arg_search_books_by_title'
 
 class ArgLibraryFactory
   def self.new_arg(command)
@@ -11,6 +15,14 @@ class ArgLibraryFactory
         ArgTakeBook.new
       when 'list_book'
         ArgListBook.new
+      when 'put_book'
+        ArgPutBook.new
+      when 'find_book'
+        ArgFindBook.new
+      when 'search_books_by_title'
+        ArgSearchBooksByTitle.new
+      when 'search_books_by_author'
+        ArgSearchBooksByAuthor.new
     end
   end
 end
