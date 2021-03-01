a7ab56ec50c70573b048ac7eea8e1c2cf7a7c700
Anggi Maisa Hadaya <anggimhadaya@gmail.com>
2021-03-01 21:34:01 +0700

    [Anggi] creating unit test for arg put book - should return an argument for put book command

---
+++ app/factory/library/arg_put_book.rb
@@ -0,0 +1,15 @@
+require './app/factory/argument'
+
+class ArgPutBook < Argument
+  def get(param)
+    params = param.split('|')
+    isbn = params[0]
+    title = params[1]
+    author = params[2..params.size-1][0].split(', ')
+    book = Book.new(isbn, title, author)
+    {
+      "shelves" => Library.instance,
+      "book" => book
+    }
+  end
+end
---
+++ spec/factory/library/arg_put_book_spec.rb
@@ -0,0 +1,25 @@
+require './app/factory/library/arg_put_book'
+
+describe ArgPutBook do
+  let(:isbn) { '9780747532743' }
+  let(:title) { 'Harry Potter 1' }
+  let(:authors) { ['J. K. Rowling', 'Hanry Ham'] }
+  let(:book_string) { "#{isbn}|#{title}|J. K. Rowling, Hanry Ham" }
+
+  context 'get' do
+    it 'should return an argument for put book command' do
+      arg_put_book = ArgPutBook.new
+      actual = arg_put_book.get(book_string)
+      library = double
+      allow(library).to receive(:instance).and_return(Library.instance)
+      expectation = {
+        'shelves' => library.instance,
+        'book' => Book.new(isbn, title, authors)
+      }
+      result = actual['shelves'] == expectation['shelves'] &&
+               actual['book'].eql?(expectation['book'])
+
+      expect(result).to eq(true)
+    end
+  end
+end
