8b6f2bff71eeb6c3257ffed91152c095a1f95938
Anggi Maisa Hadaya <anggimhadaya@gmail.com>
2021-03-02 00:56:23 +0700

    [Anggi] refactoring build library command

--- app/command/library/build_library_command.rb
+++ app/command/library/build_library_command.rb
@@ -8,23 +8,33 @@ class BuildLibraryCommand < Command
     total_shelf = shelf_size[0].to_i
     total_row = shelf_size[1].to_i
     total_column = shelf_size[2].to_i
-    message = Array.new
 
-    generate_shelves(message, total_shelf, total_row, total_column)
+    generate_shelves(total_shelf, total_row, total_column)
   end
 
   private
 
-  def generate_shelves(message, total_shelf, total_row, total_column)
-    for shelf in 1..total_shelf
-      for row in 1..total_row
-        for column in 1..total_column
-          Library.instance.add(Shelf.new(shelf, row, column))
+  def generate_shelves(total_shelf, total_row, total_column)
+    message = Array.new
+    shelf_size = Shelf.new(total_shelf, total_row, total_column)
+    if shelf_size.valid?
+      for shelf in 1..total_shelf
+        for row in 1..total_row
+          for column in 1..total_column
+            Library.instance.add(Shelf.new(shelf, row, column))
+          end
         end
+        message << "Shelf #{shelf} with #{total_row} rows and #{total_column} columns is added"
       end
-      message << "Shelf #{shelf} with #{total_row} rows and #{total_column} columns is added"
+      return {
+        'message' => message,
+        'status' => true
+      }
     end
-    message
+    {
+      'message' => "Library size cannot more than 99 and less than 1",
+      'status' => false
+    }
   end
 
 end
--- spec/command/library/build_library_command_spec.rb
+++ spec/command/library/build_library_command_spec.rb
@@ -15,7 +15,17 @@ describe BuildLibraryCommand do
       actual = build_library_command.execute(args)
       expectation = ["Shelf 1 with #{total_row} rows and #{total_column} columns is added",
                      "Shelf 2 with #{total_row} rows and #{total_column} columns is added"]
-      expect(actual).to eq(expectation)
+      expect(actual['message']).to eq(expectation)
+    end
+    it 'should return failed message when the shelf size is overflow' do
+      args = {
+        "shelves" => Array.new,
+        "shelf_size" => "2|100|0"
+      }
+      build_library_command = BuildLibraryCommand.new
+      actual = build_library_command.execute(args)
+      expectation = "Library size cannot more than 99 and less than 1"
+      expect(actual['message']).to eq(expectation)
     end
   end
 end
