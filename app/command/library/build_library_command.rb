b622c9614cfaa498f47b0fa26391e9da26e717d6
Anggi Maisa Hadaya <anggimhadaya@gmail.com>
2021-03-02 00:06:44 +0700

    [Anggi] refactoring build library command

--- app/command/library/build_library_command.rb
+++ app/command/library/build_library_command.rb
@@ -4,23 +4,22 @@ require './app/model/shelf'
 
 class BuildLibraryCommand < Command
   def execute(args)
-    shelves = args['shelves']
     shelf_size = args['shelf_size'].split('|')
     total_shelf = shelf_size[0].to_i
     total_row = shelf_size[1].to_i
     total_column = shelf_size[2].to_i
     message = Array.new
 
-    generate_shelves(shelves, message, total_shelf, total_row, total_column)
+    generate_shelves(message, total_shelf, total_row, total_column)
   end
 
   private
 
-  def generate_shelves(shelves, message, total_shelf, total_row, total_column)
+  def generate_shelves(message, total_shelf, total_row, total_column)
     for shelf in 1..total_shelf
       for row in 1..total_row
         for column in 1..total_column
-          shelves << BookShelf.new(Shelf.new(shelf, row, column))
+          Library.instance.add(Shelf.new(shelf, row, column))
         end
       end
       message << "Shelf #{shelf} with #{total_row} rows and #{total_column} columns is added"
