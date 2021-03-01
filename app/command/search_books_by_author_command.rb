3635f3d0b19854cec03e33d04cb3314ef46bd0e1
Anggi Maisa Hadaya <anggimhadaya@gmail.com>
2021-03-01 12:51:18 +0700

    [Anggi] creating unit test for search books by author command class - should print filtered list of book if book author is found

---
+++ app/command/search_books_by_author_command.rb
@@ -0,0 +1,17 @@
+require './app/command'
+
+class SearchBooksByAuthorCommand < Command
+  def execute(args)
+    shelves = args['shelves']
+    book_author = args['book_author']
+    shelves_book_filtered = Array.new
+
+    shelves.each do |book_shelf|
+      if book_shelf.book.include_author? book_author
+        shelves_book_filtered << book_shelf.print
+      end
+    end
+    return "Book not found!" unless !shelves_book_filtered.empty?
+    shelves_book_filtered
+  end
+end
