d8952ccb87ef4410bc378b48e22159b675195bad
Anggi Maisa Hadaya <anggimhadaya@gmail.com>
2021-03-02 01:40:09 +0700

    [Anggi] creating main system of my library

---
+++ app/main.rb
@@ -0,0 +1,35 @@
+require './app/model/library'
+require './app/command/executor'
+
+def new_line
+  25.times{ puts "" }
+end
+
+executor = Executor.new
+
+loop do
+  puts "Input your command down here: "
+  commands = gets.chomp.split("|")
+  command = commands[0]
+  break if command.eql? 'exit'
+  param = commands[1..commands.size-1].join('|')
+  puts "You should build a library (ex. build_library|total_shelves|total_rows|total_columns)" unless command.eql? 'build_library'
+  @result = executor.execute(command, param)
+  puts @result['message'] if command.eql? 'build_library'
+  break if @result['status'].eql? true
+end
+
+if @result['status'].eql? true
+  loop do
+    puts "Input your command down here: "
+    commands = gets.chomp.split("|")
+    command = commands[0]
+    break if command.eql? 'exit'
+    param = commands[1..commands.size-1].join('|')
+    puts "You cannot build a library twice" if command.eql? 'build_library'
+
+    if !command.eql? 'build_library'
+      puts executor.execute(command, param)['message']
+    end
+  end
+end
