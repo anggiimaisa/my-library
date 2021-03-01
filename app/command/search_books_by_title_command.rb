8c7024aeb87c408673be44d4bfe04311d6988ecc
Anggi Maisa Hadaya <anggimhadaya@gmail.com>
2021-03-01 12:43:06 +0700

    [Anggi] creating unit test for search book by title command class - should print filtered list of book if book title is found

---
+++ app/command/search_books_by_title_command.rb
@@ -0,0 +1,16 @@
+require './app/command'
+
+class SearchBooksByTitleCommand < Command
+  def execute(args)
+    shelves = args['shelves']
+    book_title = args['book_title']
+    shelves_book_filtered = Array.new
+
+    shelves.each do |book_shelf|
+      if book_shelf.book.include_title? book_title
+        shelves_book_filtered << book_shelf.print
+      end
+    end
+    shelves_book_filtered
+  end
+end
---
+++ spec/command/search_books_by_title_command_spec.rb
@@ -0,0 +1,36 @@
+require './app/command/search_books_by_title_command'
+
+describe FindBookCommand do
+  let(:title_to_be_searched) { 'Harry Potter' }
+  let(:first_isbn) { '9780747532743' }
+  let(:first_title) { 'Harry Potter 1' }
+  let(:first_author) { ['J. K. Rowling'] }
+  let(:second_isbn) { '9780747532826' }
+  let(:second_title) { 'Clean Code' }
+  let(:second_author) { ['Robert Cecil Martin'] }
+  let(:first_book) { Book.new(first_isbn, first_title, first_author) }
+  let(:second_book) { Book.new(second_isbn, second_title, second_author) }
+  let(:first_shelf) { Shelf.new(1, 1, 1) }
+  let(:second_shelf) { Shelf.new(1, 1, 2) }
+  let(:third_shelf) { Shelf.new(1, 1, 3) }
+  let(:first_book_shelf) { BookShelf.new(first_shelf, first_book) }
+  let(:second_book_shelf) { BookShelf.new(second_shelf, second_book) }
+  let(:third_book_shelf) { BookShelf.new(third_shelf, first_book) }
+
+  context 'execute' do
+    it 'should print filtered list of book if book title is found' do
+      args = {
+        "shelves" => [
+          first_book_shelf, second_book_shelf, third_book_shelf
+        ],
+        "book_title" => title_to_be_searched
+      }
+      search_books_by_title_command = SearchBooksByTitleCommand.new
+      actual = search_books_by_title_command.execute(args)
+      expectation = ["010101: #{first_isbn} | #{first_title} | #{first_author.join(', ')}",
+                     "010103: #{first_isbn} | #{first_title} | #{first_author.join(', ')}"]
+      expect(actual).to eq(expectation)
+    end
+  end
+end
+
