45961181449a7a5d491ce2875dfac28fb8206a4e
Anggi Maisa Hadaya <anggimhadaya@gmail.com>
2021-03-02 19:33:00 +0700

    [Anggi] add validation when input command nil

--- app/main.rb
+++ app/main.rb
@@ -3,16 +3,25 @@ require './app/command/executor'
 
 executor = Executor.new
 
+@result = {
+  'message' => '',
+  'status' => false
+}
+
 loop do
   puts "Input your command down here: "
   commands = gets.chomp.split("|")
   command = commands[0]
   break if command.eql? 'exit'
-  param = commands[1..commands.size-1].join('|')
-  puts "You should build a library (ex. build_library|total_shelves|total_rows|total_columns)" unless command.eql? 'build_library'
-  @result = executor.execute(command, param)
-  puts @result['message'] if command.eql? 'build_library'
-  break if @result['status'].eql? true
+  if !command.nil?
+    param = commands[1..commands.size-1].join('|')
+    puts "You should build a library (ex. build_library|total_shelves|total_rows|total_columns)" unless command.eql? 'build_library'
+    @result = executor.execute(command, param)
+    puts @result['message'] if command.eql? 'build_library'
+    break if @result['status'].eql? true
+  else
+    puts 'Program cannot recognize the command'
+  end
 end
 
 if @result['status'].eql? true
@@ -21,11 +30,16 @@ if @result['status'].eql? true
     commands = gets.chomp.split("|")
     command = commands[0]
     break if command.eql? 'exit'
-    param = commands[1..commands.size-1].join('|')
-    puts "You cannot build a library twice" if command.eql? 'build_library'
+    if !command.nil?
+      param = commands[1..commands.size-1].join('|')
+      puts "You cannot build a library twice" if command.eql? 'build_library'
 
-    if !command.eql? 'build_library'
-      puts executor.execute(command, param)['message']
+      if !command.eql? 'build_library'
+        puts executor.execute(command, param)['message']
+      end
+    else
+      puts 'Program cannot recognize the command'
     end
+
   end
 end
